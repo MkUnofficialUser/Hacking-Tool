f3a609f59d611b64491af770760a8550a37677de
MkUnofficialUser <165890073+MkUnofficialUser@users.noreply.github.com>
2024-12-13 10:23:49 +0530

    Add files via upload

---
+++ helpmsg.py
@@ -0,0 +1,17 @@
+logohelpmsg = """\n\n
+
+\nYou Can Contact Me For Your Help.\n
+
+\nCurrently I Cannot Create Any Helping Messages For This Program.\n
+
+\nComplete The Steps In This Program Without Any Mistake.\n
+
+\nThen You Will Reach Your Goal, I Mean You Get The Tools You Need.\n
+
+\nAfter Payment Contact Me And Share Screenshot Of Payment.\n
+
+\nYou Send Me The Screenshot Of Payment Then I Will Give You The Secret Key.\n
+
+\n In This Program You Can Choose Your Desired Option By Just Inputting Numbers.\n
+
+\n\n"""
---
+++ requirements.sh
@@ -0,0 +1,13 @@
+apt update -y
+pkg update -y
+apt upgrade -y
+pkg upgrade -y
+pkg install git -y
+pip install qrcode
+pip install requests
+pip install colorama
+pkg install python -y
+pkg install nodejs -y
+pkg install python-pip
+pkg install python2 -y
+pkg install python3 -y
